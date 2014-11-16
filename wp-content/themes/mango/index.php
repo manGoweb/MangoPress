4efcc65d3a96721ba84051144d71ea33021b9360
ViliamKopecky <vk@viliamkopecky.cz>
2014-11-16 23:49:45 +0100

    Use Latte utils for templating

--- wp-content/themes/mango/index.php
+++ wp-content/themes/mango/index.php
@@ -1,11 +1,3 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-<meta charset="UTF-8">
-<title>Mango template</title>
-</head>
-<body>
-<h1>Hello world!</h1>
-<p>This is a theme template.</p>
-</body>
-</html>
+<?php
+
+view('index', array( 'greeting' => 'Hello' ));
