755d84f9249a76d6ece5592efd12c186a5327701
ViliamKopecky <vk@viliamkopecky.cz>
2014-11-24 22:33:33 +0100

    Update loading theme utils via array list

--- theme/functions.php
+++ theme/functions.php
@@ -4,4 +4,7 @@ define('THEME_DIR', dirname(__FILE__));
 define('THEME_UTILS_DIR', THEME_DIR . '/utils');
 define('THEME_VIEWS_DIR', THEME_DIR . '/views');
 
-require THEME_UTILS_DIR . '/latte.php';
+foreach(array( 'latte', 'theme-uri' ) as $util_name) {
+	require THEME_UTILS_DIR . "/$util_name.php";
+}
+
