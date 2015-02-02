346b466699922fdf44872b2aa326d55d9e20bdf0
ViliamKopecky <vk@viliamkopecky.cz>
2015-02-02 16:12:54 +0100

    Show maintenance page

--- public/index.php
+++ public/index.php
@@ -1,9 +1,8 @@
 <?php
 
-// run, WordPress, run!
-if(file_exists(dirname(__FILE__) . '/wp-core/index.php') && file_exists(dirname(__FILE__) . '/vendor/autoload.php')) {
-	require dirname(__FILE__) . '/wp-core/index.php';
-} else {
-	header('Content-Type: text/plain;charset=utf-8');
-	die("Please install Composer dependencies.\n\nhttp://getcomposer.org");
+if(file_exists(__DIR__ . '/maintenance.php')) {
+	require __DIR__ . '/maintenance.php';
 }
+
+// run, WordPress, run!
+require dirname(__FILE__) . '/wp-core/index.php';
