2000ee8447a24be615bb5d87a665c771d523485e
ViliamKopecky <vk@viliamkopecky.cz>
2015-02-02 16:19:07 +0100

    Drop mango with proxy

---
+++ theme/utils/mango.php
@@ -0,0 +1,8 @@
+<?php
+
+$View['mangoSnippet'] = NULL;
+$mangoSnippetPath = BASE_DIR . '/.mango-snippet.html';
+
+if(!empty($App->parameters['mango']) && file_exists($mangoSnippetPath)) {
+	$View['mangoSnippet'] = Nette\Utils\Html::el()->setHtml(file_get_contents($mangoSnippetPath));
+}
