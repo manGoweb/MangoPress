dc404b07ba2f4331802129b204f58b6e253a7ba0
ViliamKopecky <vk@viliamkopecky.cz>
2015-02-02 16:10:11 +0100

    Fix basePath detection

--- app/misc.php
+++ app/misc.php
@@ -3,4 +3,15 @@
 $Request = $container->httpRequest;
 
 $Url = $Request->url;
-$Url->setScriptPath(str_replace('\\', '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(WWW_DIR))) . '/');
+$cwd = strtolower(realpath('.'));
+$wpdir = strtolower(realpath(WP_DIR));
+
+if(substr($cwd, 0, strlen($wpdir)) === $wpdir) {
+	$scriptPath = '/';
+	$basePathTest = str_replace('\\', '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(WWW_DIR))) . '/';
+	if(strpos($Url->getPath(), $basePathTest) === FALSE) {
+		$Url->setScriptPath($scriptPath);
+	} else {
+		$Url->setScriptPath($basePathTest);
+	}
+}
