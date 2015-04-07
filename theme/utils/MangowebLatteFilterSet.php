a156da5aa1c667fb1db2e8cccdaa0004796da4f1
ViliamKopecky <vk@viliamkopecky.cz>
2015-04-07 15:13:39 +0200

    Add support for dynamically adding Latte filters

--- theme/utils/MangowebLatteFilterSet.php
+++ theme/utils/MangowebLatteFilterSet.php
@@ -1,10 +1,25 @@
 <?php
 
+/*
+
+// Example how to add dynamic filter
+
+MangoFilters::$set['foo'] = function($str) {
+	return "foo $str bar";
+};
+
+*/
+
 class MangowebLatteFilterSet {
+
+	public static $set = [];
 
 	public static function install(Latte\Engine $latte) {
 		$me = new static;
 		$latte->addFilter('emphasize', array($me, 'filterEmphasize'));
+		foreach(self::$set as $filter_name => $callback) {
+			$latte->addFilter($filter_name, $callback);
+		}
 	}
 
 	public function filterEmphasize($value) {
@@ -12,3 +27,5 @@ class MangowebLatteFilterSet {
 	}
 
 }
+
+class MangoFilters extends MangowebLatteFilterSet {}
