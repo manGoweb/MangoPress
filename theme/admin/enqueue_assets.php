d0f482564d5fb2e06c46217e87a9f1e59519b0f4
ViliamKopecky <vk@viliamkopecky.cz>
2015-06-05 16:31:35 +0200

    Admin side frontend utils (admin.css and admin.js)

---
+++ theme/admin/enqueue_assets.php
@@ -0,0 +1,12 @@
+<?php
+
+function mango_admin_scripts() {
+	wp_enqueue_style('mango-admin-style', get_home_url() . '/assets/styles/admin.css');
+	$script_path = '/assets/scripts/admin.js';
+	if(file_exists(WWW_DIR . $script_path)) {
+		$v = md5_file(WWW_DIR . $script_path);
+		wp_enqueue_script('mango-admin-script', get_home_url() . $script_path, null, $v, true);
+	}
+}
+
+add_action( 'admin_init', 'mango_admin_scripts' );
--- theme/functions.php
+++ theme/functions.php
@@ -6,9 +6,14 @@ setlocale(LC_ALL,  get_locale().'.utf-8');
 
 define('THEME_DIR', dirname(__FILE__));
 define('THEME_UTILS_DIR', THEME_DIR . '/utils');
+define('ADMIN_UTILS_DIR', THEME_DIR . '/admin');
 define('THEME_VIEWS_DIR', THEME_DIR . '/views');
 define('NEON_WP_DIR', __DIR__ . '/define');
 
 foreach(glob(THEME_UTILS_DIR . '/*.php') as $filename) {
+	require_once $filename;
+}
+
+if(is_admin()) foreach(glob(ADMIN_UTILS_DIR . '/*.php') as $filename) {
 	require_once $filename;
 }
