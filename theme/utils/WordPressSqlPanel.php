e55ee28b56a7272b6695b1aff08ecca594281dc4
ViliamKopecky <vk@viliamkopecky.cz>
2015-08-03 21:58:23 +0200

    rename

--- theme/utils/WordPressSqlPanel.php
+++ theme/utils/WordPressSqlPanel.php
@@ -1,13 +1,24 @@
 <?php
 
-class WordPressSqlPanel extends Nette\Object implements Nette\Diagnostics\IBarPanel {
+class WordPressSqlPanel implements Nette\Diagnostics\IBarPanel {
+
+
+	public static function init() {
+		if(!Tracy\Debugger::$productionMode) {
+			Tracy\Debugger::getBar()->addPanel(new self);
+		}
+	}
+
+
 	public function __construct() {
 		define('SAVEQUERIES', true);
 	}
+
+
 	public function getTab() {
 		global $wpdb;
 		$span = Nette\Utils\Html::el('span');
-		$src = Nette\Templating\DefaultHelpers::dataStream(file_get_contents(dirname(__FILE__) . '/wp-sql-panel-icon.png'));
+		$src = Nette\Templating\DefaultHelpers::dataStream(file_get_contents(dirname(__FILE__) . '/WordPressSqlPanel.png'));
 		$img = Nette\Utils\Html::el('img width=16', array('alt' => 'WordPress icon', 'src' => $src));
 		$span->add($img);
 		$time = 0;
@@ -18,6 +29,8 @@ class WordPressSqlPanel extends Nette\Object implements Nette\Diagnostics\IBarPa
 		$span->add(($time ? sprintf(' %0.1f ms / ', $time * 1000) : '') . $count);
 		return $span;
 	}
+
+
 	public function getPanel() {
 		global $wpdb;
 		$inner = '';
@@ -28,18 +41,18 @@ class WordPressSqlPanel extends Nette\Object implements Nette\Diagnostics\IBarPa
 			$inner .= "<tr><td>$time</td><td><code>$query</code></td></tr>";
 		}
 		$panelHtml = "
-<h1>WordPress SQL</h1>
-<div class='tracy-inner nette-DbConnectionPanel'>
-<table>
-<tr><th>Time&nbsp;ms</th><th>SQL Query</th></tr>
-".$inner."
-</table>
-</div>
-";
+		<h1>WordPress SQL</h1>
+		<div class='tracy-inner nette-DbConnectionPanel'>
+			<table>
+				<tr><th>Time&nbsp;ms</th><th>SQL Query</th></tr>
+				".$inner."
+			</table>
+		</div>
+		";
 		return $panelHtml;
 	}
+
+
 }
 
-if(!Tracy\Debugger::$productionMode) {
-	Tracy\Debugger::getBar()->addPanel(new WordPressSqlPanel);
-}
+WordPressSqlPanel::init();
