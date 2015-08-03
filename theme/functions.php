f9f47943aacc118b268463debdd082b56ace0c1f
ViliamKopecky <vk@viliamkopecky.cz>
2015-08-03 21:59:07 +0200

    include latte utils from new destination

--- theme/functions.php
+++ theme/functions.php
@@ -10,6 +10,8 @@ define('ADMIN_UTILS_DIR', THEME_DIR . '/admin');
 define('THEME_VIEWS_DIR', THEME_DIR . '/views');
 define('NEON_WP_DIR', __DIR__ . '/define');
 
+require_once __DIR__ . '/latte/init.php';
+
 foreach(glob(THEME_UTILS_DIR . '/*.php') as $filename) {
 	require_once $filename;
 }
