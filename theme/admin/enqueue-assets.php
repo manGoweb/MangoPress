a3ccb0f28e8aea396a8065eec3a757612a233598
ViliamKopecky <vk@viliamkopecky.cz>
2015-08-23 16:11:31 +0200

    add netteForms to admin section

--- theme/admin/enqueue-assets.php
+++ theme/admin/enqueue-assets.php
@@ -5,6 +5,7 @@ function mango_admin_scripts() {
 	$script_path = '/assets/scripts/admin.js';
 	if(file_exists(WWW_DIR . $script_path)) {
 		$v = md5_file(WWW_DIR . $script_path);
+		wp_enqueue_script('nette-forms-script', 'https://nette.github.io/resources/js/netteForms.min.js', null, $v, true);
 		wp_enqueue_script('mango-admin-script', get_home_url() . $script_path, null, $v, true);
 	}
 }
