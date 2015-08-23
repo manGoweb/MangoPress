bea8f6608a82b8c636b8e5628901f48daa54cdd6
ViliamKopecky <vk@viliamkopecky.cz>
2015-08-23 16:21:02 +0200

    Implemented macro set

--- theme/latte/utils/MangowebLatteMacroSet.php
+++ theme/latte/utils/MangowebLatteMacroSet.php
@@ -4,12 +4,15 @@ use Latte\PhpWriter,
     Latte\Macros\MacroSet,
     Latte\MacroNode;
 
+use Nette\Utils\Strings;
+
 class MangowebLatteMacroSet extends MacroSet {
 
 	public static function install(Latte\Compiler $compiler) {
 		$me = new static($compiler);
 
 		$me->addMacro('loop', array($me, 'macroLoop'), array($me, 'macroLoopEnd'));
+		$me->addMacro('set', array($me, 'macroSet'));
 	}
 
 	public function macroLoop(MacroNode $node, PhpWriter $writer) {
@@ -19,6 +22,14 @@ class MangowebLatteMacroSet extends MacroSet {
 
 	public function macroLoopEnd(MacroNode $node, PhpWriter $writer) {
 		return $writer->write('}wp_reset_postdata();');
+	}
+
+	public function macroSet(MacroNode $node, PhpWriter $writer) {
+		$parts = Strings::replace($node->args, '~(\\s*(=>|=)\\s*|\\s+)~', '~~~', 1);
+		$parts = Strings::split($parts, '/~~~/');
+		$variable = $parts[0];
+		$rest = $parts[1];
+		return $writer->write($variable . ' = %modify(' . $rest . ')');
 	}
 
 }
