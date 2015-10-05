03448f30dd0388c781ca571477304c5f36874b37
ViliamKopecky <vk@viliamkopecky.cz>
2015-10-05 14:38:45 +0200

    Universal Analytics utils

---
+++ theme/utils/universal-analytics.php
@@ -0,0 +1,20 @@
+<?php
+
+function analytics($googleAnalyticsId = null) {
+	global $App;
+	if(!$googleAnalyticsId) {
+		if(!empty($App->parameters['UA'])) {
+			$googleAnalyticsId = $App->parameters['UA'];
+		}
+	}
+
+	if($googleAnalyticsId && $googleAnalyticsId !== 'UA-XXXXXXX-X') {
+		return Nette\Utils\Html::el()->setHtml("<script>(function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+(i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+})(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+ga('create', '{$googleAnalyticsId}', 'auto'); ga('send', 'pageview');</script>");
+	}
+
+	return '';
+}
