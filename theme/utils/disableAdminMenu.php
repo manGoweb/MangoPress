c0708d90e15e2e236322137269dbdb5fab5cc705
ViliamKopecky <vk@viliamkopecky.cz>
2015-10-23 14:24:51 +0200

    deprecated disableAdminMenu.php

--- theme/utils/disableAdminMenu.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-function remove_menus(){
-	remove_menu_page( 'edit.php' );                   //Posts
-	remove_menu_page( 'edit-comments.php' );          //Comments
-	remove_menu_page( 'themes.php' );                 //Appearance
-
-}
-add_action( 'admin_menu', 'remove_menus' );
