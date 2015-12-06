634e9f7f08f5c57f678b80746e2e75a2310b065f
ViliamKopecky <vk@viliamkopecky.cz>
2015-12-06 17:55:19 +0100

    cleanup functions.php to be freely modifiable

--- theme/functions.php
+++ theme/functions.php
@@ -1,44 +1,5 @@
 <?php
 
-use Nette\Utils\Strings;
-use Nette\Utils\ArrayHash;
-
-
-$View = new ArrayHash;
-
-$View->parameters = ArrayHash::from($App->parameters);
+require __DIR__ . '/theme-init.php';
 
 setlocale(LC_ALL,  get_locale().'.utf-8');
-
-define('THEME_DIR', dirname(__FILE__));
-define('THEME_UTILS_DIR', THEME_DIR . '/utils');
-define('ADMIN_UTILS_DIR', THEME_DIR . '/admin');
-define('API_DIR', THEME_DIR . '/api');
-define('FORMS_DIR', THEME_DIR . '/forms');
-define('THEME_VIEWS_DIR', THEME_DIR . '/views');
-define('NEON_WP_DIR', __DIR__ . '/define');
-
-require_once __DIR__ . '/latte/init.php';
-
-foreach(glob(THEME_UTILS_DIR . '/*.php') as $filename) {
-	require_once $filename;
-}
-
-$Forms = new ArrayHash;
-foreach(glob(FORMS_DIR . '/*.php') as $filename) {
-	$Forms[basename($filename, '.php')] = require_once $filename;
-}
-$View->Forms = $Forms;
-
-if(is_admin()) foreach(glob(ADMIN_UTILS_DIR . '/*.php') as $filename) {
-	require_once $filename;
-}
-
-// CSRF protection
-$App->session->start();
-
-if(Strings::startsWith($Url->pathInfo, 'api/')) {
-	$ApiRequest = Strings::split(Strings::trim($Url->pathInfo, '~/+~'), '~/~');
-	array_shift($ApiRequest);
-	require API_DIR . '/index.php';
-}
