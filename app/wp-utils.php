d59c008ea0cee19bb6bf2806a1cc2f18226bda99
ViliamKopecky <vk@viliamkopecky.cz>
2015-12-14 11:07:35 +0100

    case insensitive basePath

--- app/wp-utils.php
+++ app/wp-utils.php
@@ -9,7 +9,7 @@ $wpdir = strtolower(realpath(WP_DIR));
 if(substr($cwd, 0, strlen($wpdir)) === $wpdir) {
 	$scriptPath = '/';
 	$basePathTest = str_replace('\\', '/', str_replace(realpath($_SERVER['DOCUMENT_ROOT']), '', realpath(WWW_DIR))) . '/';
-	if(strpos($Url->getPath(), $basePathTest) === FALSE) {
+	if(strpos(strtolower($Url->getPath()), strtolower($basePathTest)) === FALSE) {
 		$Url->setScriptPath($scriptPath);
 	} else {
 		$Url->setScriptPath($basePathTest);
