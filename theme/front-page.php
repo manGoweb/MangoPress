a37564fb953b17dd30343b6f24eec45bf198f053
ViliamKopecky <vk@viliamkopecky.cz>
2016-01-04 12:01:58 +0100

    styleguide by default

--- theme/front-page.php
+++ theme/front-page.php
@@ -1,4 +1,9 @@
 <?php
 
+if(isset($_GET['styleguide'])) {
+	view('styleguide', [ 'components' => Nette\Utils\Finder::findFiles('*.latte')->from(THEME_VIEWS_DIR . '/components') ]);
+	exit;
+}
+
 // latte file has same name as this file
 view(basename(__FILE__ , '.php'), [ 'greeting' => 'Hello' ]);
