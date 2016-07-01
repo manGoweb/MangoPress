6d4b6e0201d831255c1cfcfe33f0ab969b526d45
Viliam Kopecký <vk@viliamkopecky.cz>
2016-07-01 16:01:37 +0200

    better image sizes

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -5,8 +5,8 @@ function add_image_sizes() {
 	add_image_size('xl', 1920);
 	add_image_size('l', 1280);
 	add_image_size('m', 960);
-	add_image_size('s', 720);
-	add_image_size('xs', 440);
-	add_image_size('xxs', 320);
-	add_image_size('xxxs', 240);
+	add_image_size('s', 500);
+	add_image_size('xs', 300);
+	add_image_size('xxs', 100);
+	add_image_size('xxxs', 50);
 }
