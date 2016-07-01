c5c0648ecfcfad216e0b6b7a2de7adda3c5a7b44
Viliam Kopecký <vk@viliamkopecky.cz>
2016-07-01 16:01:27 +0200

    add number and czk Latte filters

--- theme/utils/templating.php
+++ theme/utils/templating.php
@@ -1,3 +1,31 @@
 <?php
 
 MangoPressTemplating::init();
+
+// czech number format
+MangoFilters::$set['number'] = function($number, $decimal = 2) {
+	if(fmod($number, 1) == 0) {
+		$decimal = 0;
+	}
+	$sep = ',';
+	$formatted = number_format($number, $decimal, $sep, "\xC2\xA0");
+	if($decimal) {
+		$formatted = Strings::replace($formatted, '~,?0$~');
+	}
+
+	return $formatted;
+};
+
+// czech Kč number format
+MangoFilters::$set['czk'] = function($number, $decimal = 2){
+	if(fmod($number, 1) == 0) {
+		$decimal = 0;
+	}
+	$sep = ',';
+	$formatted = number_format($number, $decimal, $sep, "\xC2\xA0");
+	if($decimal) {
+		$formatted = Strings::replace($formatted, '~,?0*$~', '');
+	}
+
+	return $formatted . (!$decimal ? ',-' : '') . "\xC2\xA0Kč";
+};
