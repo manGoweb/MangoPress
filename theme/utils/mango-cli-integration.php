c790db77b589997a90c3f95782b50ab09697b01a
Filip Chalupa <chalupa.filip@gmail.com>
2016-07-15 14:52:53 +0200

    Buildstamp: mango-cli integration
    - https://github.com/manGoweb/mango-cli/blob/master/docs/config.md#buildstamp

--- theme/utils/mango-cli-integration.php
+++ theme/utils/mango-cli-integration.php
@@ -1,8 +1,18 @@
 <?php
 
+// Browsersync
 $View->mangoSnippet = NULL;
 $mangoSnippetPath = BASE_DIR . '/.mango-snippet.html';
 
 if(!empty($App->parameters['mango']) && file_exists($mangoSnippetPath)) {
 	$View->mangoSnippet = Nette\Utils\Html::el()->setHtml(file_get_contents($mangoSnippetPath));
+}
+
+
+// Mango buildstamp
+$View->buildstamp = NULL;
+$buildstampPath = BASE_DIR . '/public/assets/.buildstamp.txt';
+
+if(file_exists($buildstampPath)) {
+	$View->buildstamp = trim(file_get_contents($buildstampPath));
 }
