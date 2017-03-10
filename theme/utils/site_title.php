416062495c534f9a122c53275bb1196966a04637
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-10 13:39:45 +0100

    Site_title: do not escape twice

--- theme/utils/site_title.php
+++ theme/utils/site_title.php
@@ -1,16 +1,22 @@
 <?php
+use Nette\Utils\Html;
 
 function site_title() {
+	$title = Html::el();
+
 	if (function_exists('is_tag') && is_tag()) {
-		return 'Tag Archive for "'.$tag.'" - ' . get_bloginfo('name');
+		$title->setHtml('Tag Archive for "' . $tag . '" - ' . get_bloginfo('name'));
 	} elseif (is_archive()) {
-		return wp_title('', FALSE) . ' Archive - ' . get_bloginfo('name');
+		$title->setHtml(wp_title('', FALSE) . ' Archive - ' . get_bloginfo('name'));
 	} elseif (is_search()) {
-		return 'Search for "'.wp_specialchars($s).'" - ' . get_bloginfo('name');
+		$title->setHtml('Search for "' . wp_specialchars($s) . '" - ' . get_bloginfo('name'));
 	} elseif (!(is_404()) && (is_single()) || (is_page())) {
-		return wp_title('', FALSE) . ' - ' . get_bloginfo('name');
+		$title->setHtml(wp_title('', FALSE) . ' - ' . get_bloginfo('name'));
 	} elseif (is_404()) {
-		return 'Not Found - ' . get_bloginfo('name');
+		$title->setHtml('Not Found - ' . get_bloginfo('name'));
+	} else {
+		$title->setHtml(get_bloginfo('name'));
 	}
-	return get_bloginfo('name');
+
+	return $title;
 }
