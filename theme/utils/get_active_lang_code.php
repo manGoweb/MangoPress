127817655337e697d8caac5c6f3811cf34fd9b52
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-22 15:00:15 +0100

    Settings localization support

--- theme/utils/get_active_lang_code.php
+++ theme/utils/get_active_lang_code.php
@@ -1,5 +1,9 @@
 <?php
 
 function get_active_lang_code() {
+	global $sitepress;
+	if (isset($sitepress)) { // WPML is enabled
+		return $sitepress->get_current_language();
+	}
 	return explode('_', get_locale())[0];
 }
---
+++ theme/utils/get_setting.php
@@ -0,0 +1,13 @@
+<?php
+
+function get_setting($group, $field) {
+	static $settings;
+	if (!$settings) {
+		$settings = [];
+	}
+	if (!isset($settings[$group])) {
+		$settings[$group] = get_option($group);
+		$settings[$group]['postfix'] = str_replace('{lang}', get_active_lang_code(), $settings[$group]['postfix-format'] ?? '');
+	}
+	return $settings[$group][$field . $settings[$group]['postfix']] ?? NULL;
+}
