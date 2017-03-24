65427b517686ce9bd4b4d0ff814e8783659492e5
Filip Chalupa <chalupa.filip@gmail.com>
2017-03-24 13:18:31 +0100

    Settings: optional custom language

--- theme/utils/get_setting.php
+++ theme/utils/get_setting.php
@@ -1,13 +1,17 @@
 <?php
 
-function get_setting($group, $field) {
+function get_setting($group, $field, $lang = NULL) {
 	static $settings;
 	if (!$settings) {
 		$settings = [];
 	}
+	if (!$lang) {
+		$lang = get_active_lang_code();
+	}
 	if (!isset($settings[$group])) {
 		$settings[$group] = get_option($group);
-		$settings[$group]['postfix'] = str_replace('{lang}', get_active_lang_code(), $settings[$group]['postfix-format'] ?? '');
 	}
-	return $settings[$group][$field . $settings[$group]['postfix']] ?? $settings[$group][$field] ?? NULL;
+
+	$postfix = str_replace('{lang}', $lang, $settings[$group]['postfix-format'] ?? '');
+	return $settings[$group][$field . $postfix] ?? $settings[$group][$field] ?? NULL;
 }
