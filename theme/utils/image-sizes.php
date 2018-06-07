fe6b2a9099baa0aede2cc7aab58204a658dfceb7
Dominik Bláha <jsem@dominikblaha.cz>
2018-06-07 10:21:36 +0200

    Fix image thumb with ajax (#126)
    closes #117

--- theme/utils/image-sizes.php
+++ theme/utils/image-sizes.php
@@ -1,7 +1,5 @@
 <?php
 
-add_action('after_setup_theme', 'add_image_sizes');
-function add_image_sizes() {
 	add_image_size('xl', 1920);
 	add_image_size('l', 1280);
 	add_image_size('m', 960);
@@ -9,4 +7,3 @@ function add_image_sizes() {
 	add_image_size('xs', 300);
 	add_image_size('xxs', 100);
 	add_image_size('xxxs', 50);
-}
