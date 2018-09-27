9aaf2de72b914580ffd06633f3d03055daa3521e
Viliam Kopecky <vk@viliamkopecky.cz>
2018-09-27 11:38:35 +0200

    fix api handler hook

--- config/utils/api-handler.php
+++ config/utils/api-handler.php
@@ -4,26 +4,28 @@ require_once __DIR__ . '/baseUrl.php';
 require_once __DIR__ . '/register-image-sizes.php';
 
 $initTheme[] = function ($dir) {
-	if(file_exists($dir . "/api/index.php")) {
-		global $Req;
-		global $ApiQuery;
-		global $Payload;
+	add_action('template_redirect', function () use ($dir) {
+		if (file_exists($dir . "/api/index.php")) {
+			global $Req;
+			global $ApiQuery;
+			global $Payload;
 
-		if(!isset($Req)) {
-			return;
-		}
+			if (!isset($Req)) {
+				return;
+			}
 
-		if(!isset($Payload)) {
-			$Payload = new Nette\Utils\ArrayHash;
-		}
+			if (!isset($Payload)) {
+				$Payload = new Nette\Utils\ArrayHash;
+			}
 
-		$path = $Req->getUrl()->getRelativeUrl();
-		$parts = explode('/', trim($path, '/'));
-		if($parts[0] === 'api') {
-			array_shift($parts);
-			$ApiQuery = $parts;
-			require $dir . "/api/index.php";
-			sendPayload();
+			$path = $Req->getUrl()->getRelativeUrl();
+			$parts = explode('/', trim($path, '/'));
+			if ($parts[0] === 'api') {
+				array_shift($parts);
+				$ApiQuery = $parts;
+				require $dir . "/api/index.php";
+				sendPayload();
+			}
 		}
-	}
+	});
 };
