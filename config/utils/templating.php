68bb95b6b815f2eb04a014070e7eebb8a590b6f9
Viliam Kopecky <vk@viliamkopecky.cz>
2018-10-01 14:17:29 +0200

    global $Post in templates

--- config/utils/templating.php
+++ config/utils/templating.php
@@ -5,4 +5,9 @@ define('NBSP', "\xC2\xA0");
 $initTheme[] = function ($dir) {
 	define('THEME_VIEWS_DIR', $dir . '/views');
 	MangoPressTemplating::init();
+
+	add_action('template_redirect', function () {
+		global $View;
+		$View['Post'] = get_queried_object();
+	});
 };
