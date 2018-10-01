e2343a79084c31b18363ec7655a5bfd459006e90
Viliam Kopecky <vk@viliamkopecky.cz>
2018-10-01 14:33:18 +0200

    translate latte filter with wpml support

--- theme/utils/templating.php
+++ theme/utils/templating.php
@@ -1,1 +1,13 @@
 <?php
+
+function translateString($str, $default = null)
+{
+	if (function_exists('icl_register_string') && function_exists('icl_translate')) {
+		icl_register_string('theme-mango', $str, $default ?: $str);
+		return icl_translate('theme-mango', $str, $default ?: $str);
+	}
+
+	return $default ?: $str;
+}
+
+MangoFilters::$set['translate'] = 'translateString';
