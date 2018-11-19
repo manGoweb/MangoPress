408cc3ced8a19686df5bd1de84e8de6d3b1e9081
Viliam Kopecky <vk@viliamkopecky.cz>
2018-11-19 11:08:52 +0100

    tweaks

--- config/utils/webmanifest-handler.php
+++ config/utils/webmanifest-handler.php
@@ -4,25 +4,15 @@ require_once __DIR__ . '/baseUrl.php';
 
 $initTheme[] = function ($dir) {
 	add_action('template_redirect', function () use ($dir) {
-		if (file_exists($dir . '/webmanifest.php')) {
-			global $Req;
+		global $Req;
 
-			if (!isset($Req)) {
-				return;
-			}
+		if (!isset($Req)) {
+			return;
+		}
 
-			$path = $Req->getUrl()->getRelativeUrl();
-			$parts = explode('/', trim($path, '/'));
-			if ($parts[0] === 'manifest.webmanifest' && count($parts) === 1) {
-				$webmanifest = [];
-
-				Tracy\Debugger::$productionMode = TRUE;
-				header('Content-Type:application/json;charset=utf-8');
-
-				require $dir . '/webmanifest.php';
-
-				print(json_encode($webmanifest)) and die();
-			}
+		if ($Req->getUrl()->getPath() === '/manifest.webmanifest' && file_exists($dir . '/webmanifest.php')) {
+			$webmanifest = require($dir . '/webmanifest.php');
+			sendPayload($webmanifest);
 		}
 	});
 };
--- theme/webmanifest.php
+++ theme/webmanifest.php
@@ -1,16 +1,19 @@
 <?php
 
-$webmanifest['name'] = get_bloginfo('name');
-$webmanifest['short_name'] = get_bloginfo('name');
-$webmanifest['orientation'] = 'any';
-$webmanifest['display'] = 'standalone';
-$webmanifest['start_url'] = get_home_url();
-$webmanifest['background_color'] = '#ffffff';
-$webmanifest['theme_color'] = '#000000';
-$webmanifest['icons'] = array_map(function($size) {
-	return [
-		'src' => 'https://placekitten.com/' . $size . '/' . $size,
-		'sizes' => $size . 'x' . $size,
-		'type' => 'image/jpg',
-	];
-}, [36, 96, 192, 256, 384, 512, 1024]);
+return [
+	'name' => get_bloginfo('name'),
+	'short_name' => get_bloginfo('name'),
+	'orientation' => 'any',
+	'display' => 'standalone',
+	'start_url' => get_home_url(),
+	'background_color' => '#ffffff',
+	'theme_color' => '#000000',
+	'icons' => array_values(array_filter(array_map(function($size) {
+		$filename = 'assets/images/site-icon-' . $size . '.png';
+		return file_exists(ABSPATH . $filename) ? [
+			'src' => get_home_url() . '/' . $filename,
+			'sizes' => $size . 'x' . $size,
+			'type' => 'image/png',
+		] : null;
+	}, [36, 96, 192, 256, 384, 512, 1024]))),
+];
