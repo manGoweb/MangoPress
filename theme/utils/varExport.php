a8412312cb4e8d6c462efbe05f96fe8989733a8c
Viliam Kopecky <vk@viliamkopecky.cz>
2019-02-08 11:03:39 +0100

    add basic styleguide

---
+++ theme/utils/varExport.php
@@ -0,0 +1,26 @@
+<?php
+
+function varExport($var, $indent="")
+{
+	switch (gettype($var)) {
+		case 'integer': case 'double': return $var;
+		case "string":
+			return '"' . addcslashes($var, "\\\$\"\r\n\t\v\f") . '"';
+		case "array":
+			$indexed = array_keys($var) === range(0, count($var) - 1);
+			$r = [];
+			foreach ($var as $key => $value) {
+				$r[] = "$indent\t"
+			. ($indexed ? "" : varExport($key) . " => ")
+			. varExport($value, "$indent\t");
+			}
+			return "[\n" . implode(",\n", $r) . "\n" . $indent . "]";
+		case "boolean":
+			return $var ? "TRUE" : "FALSE";
+		default:
+			if ($var instanceof \Nette\Utils\Html) {
+				return 'safe("' . addslashes((string) $var) . '")';
+			}
+			return var_export($var, true);
+	}
+}
