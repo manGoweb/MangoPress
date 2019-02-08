75eb24d07ca94afe444aa71d490b45c6172dfeca
Viliam Kopecky <vk@viliamkopecky.cz>
2019-02-08 11:03:51 +0100

    WPML helpers

--- theme/utils/templating.php
+++ theme/utils/templating.php
@@ -1,13 +1,5 @@
 <?php
 
-function translateString($str, $default = null)
-{
-	if (function_exists('icl_register_string') && function_exists('icl_translate')) {
-		icl_register_string('theme-mango', $str, $default ?: $str);
-		return icl_translate('theme-mango', $str, $default ?: $str);
-	}
-
-	return $default ?: $str;
-}
-
-MangoFilters::$set['translate'] = 'translateString';
+MangoFilters::$set['foo'] = function () {
+	return "bar";
+};
