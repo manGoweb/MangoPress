1f2a12258d3fe87fe0efd16bf12317ca4e5a1a48
Viliam Kopecky <vk@viliamkopecky.cz>
2019-05-13 15:07:33 +0200

    Add email helpers

---
+++ theme/emails/styles.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+	'primaryAltColor' => '#34495e',
+	'whiteColor' => '#ffffff',
+	'lightColor' => '#f6f6f6',
+	'darkColor' => '#999999',
+
+	'btnBgColor' => '#3498db',
+	'btnTextColor' => '#ffffff',
+];
