9e4d7b5f4000fdc8decd528ba9102daf0026cd44
Viliam Kopecky <vk@viliamkopecky.cz>
2019-07-15 09:58:38 +0200

    Update updater

--- bin/update.sh
+++ bin/update.sh
@@ -2,11 +2,14 @@
 set -euo pipefail
 IFS=$'\n\t'
 
+yarn upgrade-interactive
+
+composer update
+
 npm i -g git+ssh://git@mangoweb.org:libs/mu-plugins.git || true
 mup update || true
 mup install || true
 
-composer update
-./vendor/bin/wp core update
-./vendor/bin/wp plugin update --all
-./vendor/bin/wp rewrite flush
+vendor/bin/wp plugin deactivate disable-wordpress-updates
+vendor/bin/wp core update
+vendor/bin/wp plugin update --all
