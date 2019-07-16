360c0896868c1cb4d889f369e4fc6cea08c203f3
Viliam Kopecky <vk@viliamkopecky.cz>
2019-07-16 14:40:31 +0200

    Update scripts-base

--- theme/scripts/wp-admin.ts
+++ theme/scripts/wp-admin.ts
@@ -3,10 +3,10 @@ import ExampleAdminPage from './adminComponents/ExampleAdminPage'
 import ExampleMetabox from './adminComponents/ExampleMetabox'
 import ExampleToolbarButton from './adminComponents/ExampleToolbarButton'
 import PageRolesMetabox from './adminComponents/PageRolesMetabox'
-import { Shapes } from '@mangoweb/scripts-base/lib/components/Shapes'
+import { Shapes } from '@mangoweb/scripts-base'
 import './plugins'
 
 initializeComponents(
-	[Shapes, ExampleToolbarButton, PageRolesMetabox, ExampleAdminPage, ExampleMetabox] as any,
+	[Shapes, ExampleToolbarButton, PageRolesMetabox, ExampleAdminPage, ExampleMetabox],
 	'initAdminComponents'
 )
