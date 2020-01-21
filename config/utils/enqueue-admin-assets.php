e09502e322d8d818d105b5aaed4a47ffe49a7c67
Viliam Kopecky <vk@viliamkopecky.cz>
2020-01-21 16:57:27 +0100

    MP: Gutenberg support

--- config/utils/enqueue-admin-assets.php
+++ config/utils/enqueue-admin-assets.php
@@ -8,7 +8,13 @@ $initTheme[] = function ($dir) {
 		$basePath = WP_SITEURL;
 		add_editor_style($basePath.'/assets/styles/'.getBuildstamp().'wp-editor.css');
 		wp_enqueue_style('mango-styles-admin', $basePath.'/assets/styles/wp-admin.css', null, getBuildstamp());
-		wp_enqueue_script('mango-scripts-admin', $basePath.'/assets/scripts/wp-admin.js', ['jquery'], getBuildstamp(), true);
+		wp_enqueue_script('mango-scripts-admin', $basePath.'/assets/scripts/wp-admin.js', ['jquery', 'wp-element'], getBuildstamp(), true);
+	});
+
+	add_action('enqueue_block_editor_assets', function () {
+		$basePath = WP_SITEURL;
+		wp_enqueue_style('mango-styles-gutenberg', $basePath.'/assets/styles/gutenberg.css', null, getBuildstamp());
+		wp_enqueue_script('mango-scripts-gutenberg', $basePath.'/assets/scripts/gutenberg.js', ['jquery', 'wp-element', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-editor'], getBuildstamp(), true);
 	});
 
 	// allow styling, when wp admin bar might be visible
@@ -16,7 +22,7 @@ $initTheme[] = function ($dir) {
 		if(is_user_logged_in()) {
 			$basePath = WP_SITEURL;
 			wp_enqueue_style('mango-styles-admin', $basePath.'/assets/styles/wp-admin.css', null, getBuildstamp());
-			wp_enqueue_script('mango-scripts-admin', $basePath.'/assets/scripts/wp-admin.js', ['jquery'], getBuildstamp(), true);
+			wp_enqueue_script('mango-scripts-admin', $basePath.'/assets/scripts/wp-admin.js', ['jquery', 'wp-element'], getBuildstamp(), true);
 		}
 	});
 
