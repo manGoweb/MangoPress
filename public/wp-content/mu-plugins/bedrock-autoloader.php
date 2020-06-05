f5fc9837a83316e01ee139cdce907bd9c8598b19
Viliam Kopecky <vk@viliamkopecky.cz>
2020-06-05 10:40:45 +0200

    Fix bedrock-autoloader

--- public/wp-content/mu-plugins/bedrock-autoloader.php
+++ public/wp-content/mu-plugins/bedrock-autoloader.php
@@ -14,7 +14,10 @@ namespace Roots\Bedrock;
 if (!is_blog_installed()) { return; }
 
 class Autoloader {
-  private static $cache; // Stores our plugin cache and site option.
+  private static $cache = [
+		'plugins' => [],
+		'count' => 0,
+	]; // Stores our plugin cache and site option.
   private static $auto_plugins; // Contains the autoloaded plugins (only when needed).
   private static $mu_plugins; // Contains the mu plugins (only when needed).
   private static $count; // Contains the plugin count.
